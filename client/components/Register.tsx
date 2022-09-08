ec955e30e33801fa780a427c05d54f499cb59589
Trương Văn Tâm <92572825+TruongVanTamm@users.noreply.github.com>
2022-09-08 14:22:48 +0700

    cap phan phan JWT

--- client/components/Register.tsx
+++ client/components/Register.tsx
@@ -1,9 +1,11 @@
 import { Button, Checkbox, DatePicker, Form, Input, Select } from 'antd';
 import axios from 'axios';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+import ReactLoading from 'react-loading';
 
 const Register: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const onFinish = (values: any) => {
     console.log('Success:', values);
   };
@@ -15,8 +17,10 @@ const Register: React.FC = () => {
 
   const handleRegister = async (values: any) => {
     try {
+      setIsLoading(true)
       await axios.post('api/auth/signup', { ...values });
       // localStorage.setItem('firstLogin', true);
+      setIsLoading(false)
       alert('success');
     } catch (err) {
       alert('faile');
@@ -24,103 +28,114 @@ const Register: React.FC = () => {
   };
 
   return (
-    <Form
-      autoComplete="off"
-      labelCol={{ span: 10 }}
-      wrapperCol={{ span: 14 }}
-      onFinish={(values) => {
-        handleRegister(values);
-      }}
-      onFinishFailed={(error) => {
-        console.log({ error });
-      }}
-    >
-      <Form.Item
-        name="username"
-        label="User Name"
-        rules={[
-          {
-            required: true,
-            message: 'Please enter user name',
-          },
-          { whitespace: true },
-          { min: 6 },
-        ]}
-      >
-        <Input placeholder="Type your name" />
-      </Form.Item>
-
-      <Form.Item
-        name="email"
-        label="Email"
-        rules={[
-          {
-            required: true,
-            message: 'Please enter your email',
-          },
-          { type: 'email', message: 'Please enter a valid email' },
-        ]}
-        hasFeedback
-      >
-        <Input placeholder="Type your email" />
-      </Form.Item>
-
-      <Form.Item
-        name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-          },
-          { min: 6 },
-          {
-            validator: (_, value) =>
-              value && regexPassWord.test(value)
-                ? Promise.resolve()
-                : Promise.reject('Password does not match criteria.'),
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password placeholder="Type your password" />
-      </Form.Item>
-
-      <Form.Item
-        name="confirmPassword"
-        label="Confirm Password"
-        dependencies={['password']}
-        rules={[
-          {
-            required: true,
-          },
-          ({ getFieldValue }) => ({
-            validator(_, value) {
-              if (!value || getFieldValue('password') === value) {
-                return Promise.resolve();
-              }
-              return Promise.reject(
-                'The two passwords that you entered does not match.'
-              );
-            },
-          }),
-        ]}
-        hasFeedback
-      >
-        <Input.Password placeholder="Confirm your password" />
-      </Form.Item>
-
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button
-          type="primary"
-          htmlType="submit"
+    <>
+      {isLoading ? (
+        <ReactLoading
+          type={'spin'}
+          color={'red'}
+          height={'10%'}
+          width={'10%'}
+        />
+      ) : (
+        <Form
+          autoComplete="off"
+          labelCol={{ span: 10 }}
+          wrapperCol={{ span: 14 }}
+          onFinish={(values) => {
+            handleRegister(values);
+          }}
+          onFinishFailed={(error) => {
+            console.log({ error });
+          }}
         >
-          Ok
-        </Button>
-        <Button type="primary">
-          <Link href="/login">Login</Link>
-        </Button>
-      </Form.Item>
-    </Form>
+          <Form.Item
+            name="username"
+            label="User Name"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter user name',
+              },
+              { whitespace: true },
+              { min: 6 },
+            ]}
+          >
+            <Input placeholder="Type your name" />
+          </Form.Item>
+
+          <Form.Item
+            name="email"
+            label="Email"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter your email',
+              },
+              { type: 'email', message: 'Please enter a valid email' },
+            ]}
+            hasFeedback
+          >
+            <Input placeholder="Type your email" />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[
+              {
+                required: true,
+              },
+              { min: 6 },
+              {
+                validator: (_, value) =>
+                  value && regexPassWord.test(value)
+                    ? Promise.resolve()
+                    : Promise.reject('Password does not match criteria.'),
+              },
+            ]}
+            hasFeedback
+          >
+            <Input.Password placeholder="Type your password" />
+          </Form.Item>
+
+          <Form.Item
+            name="confirmPassword"
+            label="Confirm Password"
+            dependencies={['password']}
+            rules={[
+              {
+                required: true,
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    'The two passwords that you entered does not match.'
+                  );
+                },
+              }),
+            ]}
+            hasFeedback
+          >
+            <Input.Password placeholder="Confirm your password" />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button
+              type="primary"
+              htmlType="submit"
+            >
+              Ok
+            </Button>
+            <Button type="primary">
+              <Link href="/login">Login</Link>
+            </Button>
+          </Form.Item>
+        </Form>
+      )}
+    </>
   );
 };
 
